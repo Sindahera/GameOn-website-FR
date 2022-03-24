7cf70434950add80bbd273cc188c40400dff0270
Sindahera <sindaherarichard@gmail.com>
2022-03-24 23:55:41 +0100

    minor fix

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,6 +18,8 @@ const submitBtn = document.getElementById('btnSubmit');
 const confirmationBtn = document.getElementById('btnConfirmation')
 
 
+
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -26,9 +28,8 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
- 
 
-// njye Defining a function to display error message
+// Defining a function to display error message
 function printError(elemId, hintMsg) {
   document.getElementById(elemId).innerHTML = hintMsg;
   document.getElementById(elemId).classList.add('errormsg');
@@ -38,64 +39,63 @@ function printError(elemId, hintMsg) {
 
 function validate(){
 
-  //DOM elements for validation
-  const firstName = document.reserve.first.value.trim;
-  const lastName = document.reserve.last.value.trim;
-  const location = document.reserve.location.value;
-  const email = document.reserve.email.value.trim;
-  const birthDate = document.reserve.birthdate.value;
-  const tournament = document.reserve.quantity.value;
-  const terms = document.getElementById('checkbox1');
-  
-  // defining error variables with a default value
-  var firstNameErr = lastNameErr = emailErr = birthDateErr  = tournamentErr = locErr = termsErr = true;
-  
-  //validating first name with minimum of 2 characters
-  
-  if (firstName.length < 2) {
-    printError("firstNameError", "Veuillez saisir au moins 2 caractères pour votre prénom")
+//DOM elements for validation
+const firstName = document.reserve.first.value;
+const lastName = document.reserve.last.value;
+const location = document.reserve.location.value;
+const email = document.reserve.email.value;
+const birthDate = document.reserve.birthdate.value;
+const tournament = document.reserve.quantity.value;
+const terms = document.getElementById('checkbox1');
+
+// defining error variables with a default value
+var firstNameErr = lastNameErr = emailErr = birthDateErr  = tournamentErr = locErr = termsErr = true;
+
+//validating first name with minimum of 2 characters
+
+if (firstName.length < 2) {
+  printError("firstNameError", "Veuillez saisir au moins 2 caractères pour votre prénom")
+} else {
+// added a validation to make sure only alpahbetical characters are used by using a regular expression
+  var regex = /^[a-zA-Z\s]+$/;                
+  if(regex.test(firstName) === false) {
+      printError("firstNameError", "Merci d'entrer un nom valide");
   } else {
-  // added a validation to make sure only alpahbetical characters are used by using a regular expression
-    var regex = /^[a-zA-Z\s]+$/;                
-    if(regex.test(firstName) === false) {
-        printError("firstNameError", "Merci d'entrer un nom valide");
-    } else {
-        printError("firstNameError", "");
-        firstNameErr = false;
-    }
+      printError("firstNameError", "");
+      firstNameErr = false;
   }
-  
-  //validating surname with minimum of 2 characters
-  
-  if (lastName.length < 2) {
-    printError("lastNameError", "Veuillez saisir au moins 2 caractères pour votre nom de famille")
+}
+
+//validating surname with minimum of 2 characters
+
+if (lastName.length < 2) {
+  printError("lastNameError", "Veuillez saisir au moins 2 caractères pour votre nom de famille")
+} else {
+// added a validation to make sure only alpahbetical characters are used by using a regular expression
+  var regex = /^[a-zA-Z\s]+$/;                
+  if(regex.test(lastName) === false) {
+      printError("lastNameError", "Merci d'entrer un nom valide");
   } else {
-  // added a validation to make sure only alpahbetical characters are used by using a regular expression
-    var regex = /^[a-zA-Z\s]+$/;                
-    if(regex.test(lastName) === false) {
-        printError("lastNameError", "Merci d'entrer un nom valide");
-    } else {
-        printError("lastNameError", "");
-        lastNameErr = false;
-    }
+      printError("lastNameError", "");
+      lastNameErr = false;
   }
-  
-  //validating email
-  
-  if (email == "") {
-    printError("emailError", "Veuillez saisir votre e-mail")
+}
+
+//validating email
+
+if (email == "") {
+  printError("emailError", "Please type in your email")
+} else {
+  var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (regex.test(email) == false){
+    printError("emailError", "S'il vous plaît, mettez une adresse email valide")
   } else {
-    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (regex.test(email) == false){
-      printError("emailError", "S'il vous plaît, mettez une adresse email valide")
-    } else {
-        printError("emailError", "");
-        emailErr = false;
-    }
+      printError("emailError", "");
+      emailErr = false;
   }
-  //validating date of birth
-  
-  
+}
+//validating date of birth
+
 if (birthDate == "") {
   printError("birthDateError", "Vous devez entrer votre date de naissance")
 } else {
@@ -125,7 +125,7 @@ if (location == "") {
   locErr = false;
 }
 
-// validating terms and conditions has been selected
+// validating tems and conditions has been selected
 
 if (!terms.checked) {
     printError("termsError", "Vous devez cocher pour accepter les termes et conditions.");
@@ -138,8 +138,7 @@ if (!terms.checked) {
 
   if (firstNameErr || lastNameErr || emailErr || birthDateErr || tournamentErr || locErr || termsErr  == true) {
     return false;
-  } 
-  else {
+  } else {
     return true;
   }
 }
